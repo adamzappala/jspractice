fb5ad78c0c71b1c9decec8920af1740ea78bc592
Adam Zappala <orangecougar@gmail.com>
2018-10-16 13:37:13 -0600

    first commit

---
+++ numbers.js
@@ -0,0 +1,15 @@
+function range(a)
+{
+	if ( a >= 50 && a <= 99)
+	{
+		console.log("true");
+	}
+	else
+	 {
+		console.log("to bad");
+	}
+return a;
+}
+
+console.log(range("50"));
+
---
+++ vowels.js
@@ -0,0 +1,6 @@
+function vowel(count)
+{
+	
+	return count;
+}
+
