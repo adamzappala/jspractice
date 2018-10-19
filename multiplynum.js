b6cf4e96e1db615ffe6d124d01d382775eb8a438
Adam Zappala <orangecougar@gmail.com>
2018-10-19 17:24:39 -0600

    yeee

--- multiplynum.js
+++ multiplynum.js
@@ -3,8 +3,8 @@ function multiplynum(number)
   let multiply = number.reduce((accumulator, currentValue) => accumulator * currentValue);
   return multiply;
 }
-console.log((multiplynum([2,2,2])=== 8));
-console.log((multiplynum([4,4,4])=== 64));
-console.log((multiplynum([0,0,0])=== 0));
-console.log((multiplynum([4,4,4])=== 4));
-console.log((multiplynum([4,4,4])=== 233));
+console.log(multiplynum([2,2,2])=== 8);
+console.log(multiplynum([4,4,4])=== 64);
+console.log(multiplynum([0,0,0])=== 0);
+console.log(multiplynum([4,4,4])=== 4);
+console.log(multiplynum([4,4,4])=== 233);
